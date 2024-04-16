e12fa6ac79b3b03cd0b6e52147ba7bd7dd920173
narayana4devops <lakshmin41@gmail.com>
2024-04-16 21:56:31 +0800

    updating installation script

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,8 +6,8 @@ echo "userID: $userID"
 if [ $userID -ne 0 ]
 then
     echo "Please run this script with root access."
-    dnf install mysql -y
-#    exit 1 # manually exit if error comes.
+    sudo dnf install mysql -y
+    exit 1 # manually exit if error comes.
 else
     echo "You are root user"
 fi
