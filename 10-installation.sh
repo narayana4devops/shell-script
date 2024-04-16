a5985d4094d8ed28a03ecab4952eeb6af16a998c
narayana4devops <lakshmin41@gmail.com>
2024-04-16 21:51:25 +0800

    updating installation script

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,3 +2,11 @@
 userID=$(id -u)
 
 echo "userID: $userID"
+
+if [ $userID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are root user"
+fi
