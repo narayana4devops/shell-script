08ab36302af2d520ca852f3c17f7c85491cf4639
narayana4devops <lakshmin41@gmail.com>
2024-04-16 20:15:55 +0800

    adding addition script

--- 07-addition.sh
+++ 07-addition.sh
@@ -9,7 +9,7 @@ No2=$2
 count=$#
 echo "Number of variables passed: $count"
 #exit 1
-if [$count -eq 2]
+if [ $count -eq 2 ]
 then
     sum=$(($No1+$No2))
     echo "Addition of $No1 and $No2 is: $sum"
