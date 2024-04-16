8345d9080a2861aa097f5409d4c106fced472fea
narayana4devops <lakshmin41@gmail.com>
2024-04-16 19:34:01 +0800

    adding read script

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NAMES=("Lakshmi","Narayana",pathi)
+
+# size of above array is 3.
+# index are 0,1,2
+# index always starts with 0.
+
+echo "First name is: ${NAMES[0]}"
+echo "Second name is: ${NAMES[1]}"
+echo "Third name is: ${NAMES[2]}"
