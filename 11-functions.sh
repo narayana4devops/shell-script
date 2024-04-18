bf9ece252d1a8a7ce9fdc6abeb0fda5d759d0149
narayana4devops <lakshmin41@gmail.com>
2024-04-18 12:26:25 +0800

    adding functions script

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,5 +4,14 @@ user_id=$(id -u)
 timestamp=$(date +%F-%H-%M-%S)
 script_name=$(echo $0 | cut -d "." -f1)
 
+
 echo "time: $timestamp"
 echo "script_name: $script_name"
+
+if [ $user_id -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
