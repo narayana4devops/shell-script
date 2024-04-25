9695f8047cdf44368dfc845d21fc1f370a1effcd
narayana4devops <lakshmin41@gmail.com>
2024-04-25 22:35:41 +0800

    updating disk usage file

--- 17-dish_usage.sh
+++ 17-dish_usage.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 disk_usage=$(df -hT | grep xfs)
-disk_treshold=6
+disk_treshold=10
 
 while IFS= read -r line
 do
     
     usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    echo $usage
+    #echo $usage
     folder=$(echo $line | awk -F " " '{print $NF}')
-    echo $folder
+    #echo $folder
+    if [ $usage -ge $disk_treshold ]
+    then
+        echo "$folder is more than $disk_treshold, current usage is : $usage"
+    fi
 done <<< $disk_usage
